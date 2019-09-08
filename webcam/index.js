05ac9da7d4d4e55a707c15324c1f92b6eac8ceb3
jgyllinsky <jgyllinsky@users.noreply.github.com>
2019-09-07 21:09:34 -0400

    add webcam code
    adding webcam functionality

--- webcam/index.js
+++ webcam/index.js
@@ -1,6 +1,10 @@
+const webcamElement = document.getElementById('webcam');
+
+
 let net;
 
-async function app() {
+// This version of app() would predict using an image. It has been replaced with a new app() that uses a webcam instead.
+async function app_run_image() {
   console.log('Loading mobilenet..');
 
   // Load the model.
@@ -11,6 +15,49 @@ async function app() {
   const imgEl = document.getElementById('img');
   const result = await net.classify(imgEl);
   console.log(result);
+}
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+  
+  await setupWebcam();
+  while (true) {
+    const result = await net.classify(webcamElement);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
+}
+
+
+// Setup the webcam
+async function setupWebcam() {
+  return new Promise((resolve, reject) => {
+    const navigatorAny = navigator;
+    navigator.getUserMedia = navigator.getUserMedia ||
+        navigatorAny.webkitGetUserMedia || navigatorAny.mozGetUserMedia ||
+        navigatorAny.msGetUserMedia;
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({video: true},
+        stream => {
+          webcamElement.srcObject = stream;
+          webcamElement.addEventListener('loadeddata',  () => resolve(), false);
+        },
+        error => reject());
+    } else {
+      reject();
+    }
+  });
 }
 
 app();
